3aead65b8a96e3bcdc3db74320510f143e7676be
David Wayne <david.wayne@wti-solutions.com>
2020-12-20 02:47:36 -0500

    beginning of incident reporting

--- FE/layouts/default.vue
+++ FE/layouts/default.vue
@@ -66,33 +66,33 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/"
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
         },
         {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire"
+          icon: 'mdi-map',
+          title: 'Burn Map',
+          to: '/map'
         },
         {
-          icon: "mdi-map",
-          title: "Burn Map",
-          to: "/map"
+          icon: 'mdi-alert',
+          title: 'Incident Report',
+          to: '/incident'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: "burn_pit"
-    };
+      title: 'Burn Pit'
+    }
   }
-};
+}
 </script>
